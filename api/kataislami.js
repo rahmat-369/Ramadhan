d23f0199be3a9392c400909d22d201fd91c7cfd1
Rhmt codeAi engineering <rohmatulloh.3609@gmail.com>
2026-02-16 04:10:06 +0700

    Add files via upload

--- api/kataislami.js
+++ api/kataislami.js
@@ -1,1 +1,10 @@
-
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://zelapioffciall.koyeb.app/random/kataislami');
+    const data = await response.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+}
